500c50e82a781937f93552388d5adfef516d890e
andrewlreeve <andrew@andrewlreeve.com>
2022-05-16 11:01:13 +1200

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # citizen-nft-snapshot
+
 Simple script to snapshot Citizen NFT holders
+
+## Setup
+
+### Node
+
+1.  Install `nvm` ([Node Version Manager](https://github.com/nvm-sh/nvm))
+2.  `cd` to the project directory and execute the following:
+
+    ```
+    nvm install
+    nvm use
+    npm install
+    ```
+
+### Moralis API
+
+1. Create Moralis account ([Moralis](https://moralis.io/))
+2. Create new server ([Create Server](https://admin.moralis.io/servers))
+3. Copy server URL and Application ID to .env
+
+   ```
+   SERVER_URL="xxx"
+   APP_ID="xxx"
+   ```
+
+### Create Snapshot
+
+```
+npm run snapshot
+```
+
+Outputs `snapshot.json` & `total_mints.json` in root directory
+
+### Prettier
+
+This project uses [Prettier](https://prettier.io/), so please run it before checking in:
+
+```
+npm run pretty
+```
+
+See `.prettierrc` for settings.
+
+Some IDEs and editors have plugins for running Prettier.
+
+### Linting
+
+This project uses [ESLint](https://eslint.org/). Check linting before checking in:
+
+```
+npm run lint
+```
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+const Moralis = require('moralis/node');
+const { writeFile } = require('fs/promises');
+require('dotenv').config();
+
+const CONTRACT_ADDRESS = '0x7eef591a6cc0403b9652e98e88476fe1bf31ddeb';
+
+async function getAllOwners() {
+  await Moralis.start({ serverUrl: process.env.SERVER_URL, appId: process.env.APP_ID });
+
+  let cursor = null;
+  let owners = {};
+  let mints = {};
+
+  // get all owners
+  do {
+    const response = await Moralis.Web3API.token.getNFTOwners({
+      address: CONTRACT_ADDRESS,
+      chain: 'eth',
+      limit: 500,
+      cursor: cursor,
+    });
+
+    console.log(
+      `Got page ${response.page} of ${Math.ceil(response.total / response.page_size)}, ${response.total} total`,
+    );
+
+    for (const owner of response.result) {
+      let tokenType = '';
+      let mintQuantity = 0;
+
+      // determine token type and mint quantity
+      switch (owner.token_id) {
+        case '42':
+          tokenType = 'citizenNFTs';
+          mintQuantity = 1;
+          break;
+        case '69':
+          tokenType = 'foundingNFTs';
+          mintQuantity = 3;
+          break;
+        case '7':
+          tokenType = 'firstNFTs';
+          mintQuantity = 10;
+          break;
+      }
+
+      // build owners object
+      owners[owner.owner_of] = owners[owner.owner_of] || {};
+      owners[owner.owner_of][tokenType] = owner.amount;
+
+      // build mints object
+      const mintAmount = mintQuantity * owner.amount;
+      mints[owner.owner_of] = mints[owner.owner_of] + mintAmount || mintAmount;
+    }
+
+    // get next page
+    cursor = response.cursor;
+  } while (cursor != '' && cursor != null);
+
+  console.log('total owners:', Object.keys(owners).length);
+
+  // write owners to file
+  try {
+    console.log('writing owners to snapshot.json');
+    writeFile('snapshot.json', JSON.stringify(owners, null, 2));
+    console.log('writing mints to total_mints.json');
+    writeFile('total_mints.json', JSON.stringify(mints, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log('snapshot complete');
+}
+
+getAllOwners();
